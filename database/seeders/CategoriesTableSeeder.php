3fac384ba780840e57732d034ea0a8ccda687abd
emma ingurat <e.ingurat@bsmart-technology.com>
2023-01-20 22:31:26 +0300

    categories seeder

---
+++ database/seeders/CategoriesTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $categories = [
+            'name' => 'Ideas', 
+            'description' => 'Ideas'
+        ];
+
+            Category::create($categories);
+    }
+}
