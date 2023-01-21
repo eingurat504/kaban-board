309d954c6db3f82665d74b09014391327a6854a5
emma ingurat <e.ingurat@bsmart-technology.com>
2023-01-21 17:47:00 +0300

    usercontroller updates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,69 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    //
+
+    public function authenticate()
+    {
+
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 401);
+        }
+
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        if (Auth::attempt($credentials)) {
+                $user_info = [
+                    'token' => Auth::user()->createToken('MyApp')->accessToken,
+                    'user' => Auth::user()->name
+                ];
+    
+            return response()->json(['user' => $$user_info]);
+        }
+
+        return response()->json(['error' => 'Unauthorised'], 401);
+    }
+
+    /**
+     * register user
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 401);
+        }
+
+        $user_info = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+        $user = User::create($user_info);
+
+        $user_details = [
+            'token' => $user->createToken('MyApp')->accessToken,
+            'user'  => $user
+        ];
+
+        return response()->json(['user' => $user_details]);
+    }
 }
