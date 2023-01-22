91d175af1a8decd22808ce6f95019e747ce6a9f6
emma ingurat <e.ingurat@bsmart-technology.com>
2023-01-22 23:58:31 +0300

    added sanctum to controllers

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -15,7 +15,7 @@ class CategoryController extends Controller
      */
     public function __construct()
     {
-        // $this->middleware('auth:sanctum');
+        $this->middleware('auth:sanctum');
     }
 
        /**
